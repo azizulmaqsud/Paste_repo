f0bd27c90ff6814099eae4ea1e35a33c78095ace
Azizul Maqsud <81431470+azizulmaqsud@users.noreply.github.com>
2024-08-20 13:08:51 -0400

    Create aws-lb-controller.tf

---
+++ aws-lb-controller.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.5.5"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = "aug-24-eks-argo"
+  }
+}
