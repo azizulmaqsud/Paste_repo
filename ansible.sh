09ecb6d7a709d554dc1032971f700240b9ed0798
Azizul Maqsud <81431470+azizulmaqsud@users.noreply.github.com>
2024-10-13 19:12:53 -0400

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt -y full-upgrade
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install python3-pip
+sudo apt install python3-boto3
+sudo apt install ansible-core
+sudo apt-get install ansible -y
+ansible --version
